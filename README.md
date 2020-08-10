1fca2aa7ad3666dfd6e68758f06734783dc31d31
Amal M <amal@Amals-MacBook-Air.local>
2020-08-10 23:10:13 +0530

    permanant archiving

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # README
 
-### Byrogirdle
+### Geogirdle
 
-Byrogirdle is a free fantasy map making tool. It simulates the creation of an archaepelago based on the principle of post-glacial rebound.
+Geogirdle is a free fantasy map making tool. It simulates the creation of an archipelago based on the principle of post-glacial rebound.
--- app.rb
+++ app.rb
@@ -1,10 +1,4 @@
-require 'chunky_png'
+require_relative 'world_builder.rb'
 
-# Creating an image from scratch, save as an interlaced PNG
-png = ChunkyPNG::Image.new(100, 100, ChunkyPNG::Color::TRANSPARENT)
-100.times do |i|
-	100.times do |j|
-		png[i,j] = ChunkyPNG::Color.rgb(10, 20, 30)
-	end
-end
-png.save('map.png', :interlace => true)
+diaspora = WorldBuilder.new 100,100
+diaspora.draw
